6770ace745c99ab39cbb37f6c0f524510f1373d5
miiauchi <michelleauchen@gmail.com>
2026-01-23 13:47:39 -0800

    Create leds.h

---
+++ gpio-led-control/leds.h
@@ -0,0 +1,14 @@
+#ifndef leds_h
+#define leds_h
+
+void portB_en();
+void portE_en();
+void red_on();
+void green_on();
+void red_off();
+void green_off();
+void red_tog();
+void green_tog();
+void delaymicro(unsigned int x);
+
+#endif
