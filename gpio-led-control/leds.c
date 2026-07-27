a7e86986586aac25aff10092ef8c6ca65a047d95
miiauchi <michelleauchen@gmail.com>
2026-07-27 13:19:22 -0700

    Update leds.c

--- gpio-led-control/leds.c
+++ gpio-led-control/leds.c
@@ -3,41 +3,24 @@
 
 void portB_en() {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
-	GPIOB->MODER &= GPIO_MODER_MODER2_0;
+	GPIOB->MODER  &= GPIO_MODER_MODER2_0;
 	GPIOB->OTYPER &= GPIO_OTYPER_ODR_2;
-	GPIOB->PUPDR &= GPIO_PUPDR_PUPDR2;
+	GPIOB->PUPDR  &= GPIO_PUPDR_PUPDR2;
 }
 
 void portE_en() {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN;
-	GPIOE->MODER &= GPIO_MODER_MODER8_0;
+	GPIOE->MODER  &= GPIO_MODER_MODER8_0;
 	GPIOE->OTYPER &= GPIO_OTYPER_ODR_8;
-	GPIOE->PUPDR &= GPIO_PUPDR_PUPDR8;
+	GPIOE->PUPDR  &= GPIO_PUPDR_PUPDR8;
 }
 
-void red_on() {
-	GPIOB->ODR |= GPIO_ODR_ODR_2;
-}
-
-void green_on() {
-	GPIOE->ODR |= GPIO_ODR_ODR_8;
-}
-
-void red_off() {
-	GPIOB->ODR &= ~GPIO_ODR_ODR_2;
-}
-
-void green_off() {
-	GPIOE->ODR &= ~GPIO_ODR_ODR_8;
-}
-
-void red_tog() {
-	GPIOB->ODR ^= GPIO_ODR_ODR_2;
-}
-
-void green_tog() {
-	GPIOE->ODR ^= GPIO_ODR_ODR_8;
-}
+void red_on()    { GPIOB->ODR |=  GPIO_ODR_ODR_2; }
+void green_on()  { GPIOE->ODR |=  GPIO_ODR_ODR_8; }
+void red_off()   { GPIOB->ODR &= ~GPIO_ODR_ODR_2; }
+void green_off() { GPIOE->ODR &= ~GPIO_ODR_ODR_8; }
+void red_tog()   { GPIOB->ODR ^=  GPIO_ODR_ODR_2; }
+void green_tog() { GPIOE->ODR ^=  GPIO_ODR_ODR_8; }
 
 void delaymicro(unsigned int x) {
 	while(x != 0) {
