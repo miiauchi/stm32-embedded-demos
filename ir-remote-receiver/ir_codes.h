00c088a552c878a6e4d59261212976a63aaf761b
miiauchi <michelleauchen@gmail.com>
2026-07-27 13:35:40 -0700

    Create ir_codes.h

---
+++ ir-remote-receiver/ir_codes.h
@@ -0,0 +1,14 @@
+#ifndef IR_CODES_H
+#define IR_CODES_H
+
+#define IR_A      0x00ff22dd
+#define IR_B      0x00ff02fd
+#define IR_C      0x00ffc23d
+#define IR_POWER  0x00ff629d
+#define IR_UP     0x00ff9867
+#define IR_DOWN   0x00ff38c7
+#define IR_LEFT   0x00ff30cf
+#define IR_RIGHT  0x00ff7a85
+#define IR_CIRCLE 0x00ff18e7
+
+#endif
